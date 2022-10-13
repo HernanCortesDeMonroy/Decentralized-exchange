c83aea24cfca0f0c09c818dd68c7ec9fc25742cf
Rackim <110779887+HernanCortesDeMonroy@users.noreply.github.com>
2022-10-13 15:54:08 +0600

    .

---
+++ gaba-dex/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import { useState, useEffect} from 'react';
+import {ethers} from 'ethers';
+import PageButton from './components/PageButton';
+
+function App() {
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [signerAddress, setSignerAddress] = useState(undefined);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider);
+    }
+    onLoad();
+  }, [])
+
+  const getSigner = async provider => {
+    provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    setSigner(signer);
+  }
+
+  const isConnected = () => signer !== undefined;
+  const getWalletAddress = () => {
+    signer.getAddress()
+    .then(address => {
+      setSignerAddress(address) 
+    })
+  }
+
+  if(signer !== undefined) {
+    getWalletAddress()
+  }
+
+  return (
+    <div className="App">
+      <div className="appNav">
+        <div className="my-2 buttonContainer buttonContainerTop">
+          <PageButton name={"Swap"} isBold={true} />
+          <PageButton name={"Pool"} />
+          <PageButton name={"Vote"} />
+          <PageButton name={"Charts"} />
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
